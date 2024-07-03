64c9c9d60b5be4824411a31b393752ad54f3a116
nhulemis <45124251+nhulemis@users.noreply.github.com>
2024-07-03 08:35:22 +0700

    Revert "Delete alll"
    This reverts commit cfb284facac6e8290a3621a93e92a3e8b01bebd0.

---
+++ Assets/Scripts/SingleTon.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    // DAY LA CLASS SINGLETON HET
+    public class SingleTon<T> : MonoBehaviour where T : SingleTon<T>
+    {
+        private static T _instance;
+        public static T Instance => _instance;
+        [SerializeField] private bool isDontDestroyOnload;
+            
+        private void Awake()
+        {
+            // tui tao cái instance cho class này
+            // oke 
+            // Tao la A day
+            if (_instance == null)
+            {
+                _instance = (T) this; 
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+            /// Tao la B day
+            if (isDontDestroyOnload)
+            {
+                DontDestroyOnLoad(this);
+            }
+        }
+    }
+}
